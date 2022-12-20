5d2ef87c57fe4b3d99a157ca13410278c6c2709a
kevinasian122 <yanchenkai@gmail.com>
2022-12-20 13:45:24 -0500

    added image as part of the input object. Used multer to save image on local folder and to mongodb

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+/* eslint-disable no-undef */
 const express = require('express')
+const bodyParser = require('body-parser')
 require('dotenv').config() //enviornment variales
 const cors = require('cors')
 const Res = require('./models/res')
 const app = express()
-
+const path = require('path')
+const multer = require('multer')
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
-
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
@@ -15,45 +19,63 @@ app.listen(PORT, () => {
 })
 
 app.get('/api/res', (request, response) => {
-    Res.find({})
+  Res.find({})
     .then(result => {
-        response.json(result)
+      response.json(result)
     })
    
 })
 
 app.get('/api/res/:id', (request, response) => {
-    let id = request.params.id
-    Res.findById(id)
+  let id = request.params.id
+  Res.findById(id)
     .then(result => {
-        if(result){
-            response.json(result)
-        }
-        else{
-            response.status(404).end()
-        }
+      if(result){
+        response.json(result)
+      }
+      else{
+        response.status(404).end()
+      }
     })
 })
 
 app.delete('/api/res/:id', (request, response) => {
-    Res.findByIdAndRemove(request.params.id)
-    .then(result => {
-        response.status(204).end() //204 means no value sent back
+  Res.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end() //204 means no value sent back
     })
 })
+const Storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './public/images')
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + path.name(file.originalname))
+  }
+})
+const upload = multer({storage: Storage})
 
-app.post('/api/res', (request, response) => {
-    const body = request.body //this gets the object 
+app.post('/api/res', upload.single('testImage'), (req, res) => {
   
-  
-    const newRes = new Res({
-      name: body.name,
-      rating: body.rating,
-      timesVisited: body.timesVisited,
-      comments: body.comments,
-    })
-    newRes.save().then(result => {
-        response.json(result)
-    })
-  
+  const body = req.body //this gets the object
+    
+  const newRes = new Res({
+    name: body.name,
+    rating: body.rating,
+    timesVisited: body.timesVisited,
+    comments: body.comments,
+    image:{
+      data: req.file.filename,
+      contentType:'image/png'
+    }
   })
+  newRes.save().then(result => {
+    res.json(result)
+  })
+    
+  
+  
+  
+  
+})
+
--- models/res.js
+++ models/res.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 
 
-mongoose.connect(url)
-  .then(result => {
+mongoose.connect(url, { useNewUrlParser: true })
+  .then(() => {
     console.log('connected')
   })
   .catch((error) => {
@@ -12,32 +13,37 @@ mongoose.connect(url)
   })
 
 const resSchema = new mongoose.Schema({ //mongoose schema
-    name: { 
-        type: String,
-        required: true
-    },
-    rating:{
-        type:Number,
-        required: true
-    },
-    timesVisited:{
-        type:Number,
-        required: true
-    },
-    comments:{
-        type:String,
-        required: true
-    }
-  })
+  name: { 
+    type: String,
+    required: true
+  },
+  rating:{
+    type:Number,
+    required: true
+  },
+  timesVisited:{
+    type:Number,
+    required: true
+  },
+  comments:{
+    type:String,
+    required: true
+  },
+  image:{
+    // eslint-disable-next-line no-undef
+    data: Buffer,
+    contentType: String
+  }
+})
 
 
 
 resSchema.set('toJSON', { 
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-  module.exports = mongoose.model('Res', resSchema)
+module.exports = mongoose.model('Res', resSchema)
